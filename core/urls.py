b4536fb9e85f1fffe23fa7de1a447dcc16bcccb9
Gsbudoia <Gsbudoia@gmail.com>
2025-12-12 15:41:32 -0300

    Corrige link de Meus Pets na home para cliente

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('novo/', views.customer_add, name='customer_add'),
-    
-    # NOVA ROTA PARA O CLIENTE FINAL:
-    path('meus-pets/', views.my_pets, name='my_pets'),
 
+    path('', views.index, name='home'),
+    path('novo/', views.customer_add, name='customer_add'),
+    path('meus-pets/', views.my_pets, name='my_pets'),
     path('meus-pets/adicionar/', views.cadastrar_pet, name='cadastrar_pet'),
 ]

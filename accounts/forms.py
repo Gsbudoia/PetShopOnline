6969a27045689500ff3b6cd248e484a00aa02451
Gsbudoia <Gsbudoia@gmail.com>
2025-12-12 15:04:05 -0300

    Remove enctype do form de pets e melhora visual

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,15 +3,21 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class ClienteSignupForm(UserCreationForm):
-    # Adicionamos os campos que faltam para o modelo Customer
     full_name = forms.CharField(label="Nome Completo", max_length=100)
     phone = forms.CharField(label="Telefone", max_length=20)
     email = forms.EmailField(label="E-mail", required=True)
 
     class Meta:
         model = CustomUser
-        # Aqui definimos quais campos aparecem no HTML
+        # Aqui é definido quais campos aparecem no HTML
         fields = ('email', 'full_name', 'phone')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Percorre todos os campos e adiciona o estilo do Bootstrap
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
 
     def save(self, commit=True):
         # 1. Salva os dados no objeto User (na memória)
